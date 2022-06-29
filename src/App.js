5ffd1fb070f251eb662c8044e84384fef1deb7bf
Ioana Antonesei Pana <ioana.pana.a@gmail.com>
2022-06-29 23:04:34 +0200

    Connect to web3 & get account and balance

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
-import logo from './logo.svg';
+import Web3 from 'web3';
+import {useState, useEffect} from "react";
 import './App.css';
 
 function App() {
+
+  const [account, setAccount] = useState();
+  const [network, setNetwork] = useState();
+  const [balance, setBalance] = useState();
+  const web3 = new Web3(Web3.givenProvider || 'http://localhost:7545');
+
+
+  useEffect(() => {
+    loadAccounts();
+  })
+
+  //load balance every time there is a change with the account
+  useEffect(() => {
+    loadBalance();
+  }, [account])
+
+  //requests accounts from givenProvider OR Ganache localhost and set the account with the first value
+  async function loadAccounts() {
+    const accounts = await web3.eth.requestAccounts();
+    setAccount(accounts[0])
+  }
+
+  // get network and balange of the account
+  async function loadBalance() {
+    const network = await web3.eth.net.getNetworkType();
+    const balance = await web3.eth.getBalance(account);
+    setNetwork(network);
+    setBalance(balance);
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Yesssss
-        </a>
+   
+          Decentralized First App 
+          <p>Your account : ({account})</p>
+          <p>Your balance ({network}) :  {balance}</p>
+              
       </header>
     </div>
   );
